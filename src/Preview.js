52af9f8c1781e35d0de54b6e58f8a8ff1668a6ac
Artie Negron <anegroncsu@gmail.com>
2019-03-22 15:08:17 -0400

    Finished drag and drop functions for text on preview image.

--- src/Preview.js
+++ src/Preview.js
@@ -1,44 +1,78 @@
 import React, { Component } from "react";
+import { once } from "cluster";
 
 class Preview extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       isTopDragging: false,
       isBottomDragging: false,
       topTextTop: 0,
       topTextLeft: 0,
-      bottomTextTop: 0,
-      bottomTextLeft: 0
-    }
+      bottomTextBottom: 0,
+      bottomTextLeft: 0,
+      listener: true,
+      bottomListener: true
+    };
   }
 
   onMouseDown = e => {
-    this.setState({isTopDragging: true});
-    document.addEventListener('mousemove', (e) => this.onMouseMove(e));
-  }
+    this.setState({ isTopDragging: true });
+    if(this.state.listener == true){
+      document.addEventListener("mousemove", e => this.onMouseMove(e), this.setState({listener: false}));
+    }
+  };
 
   onMouseUp = e => {
-    console.count();
-    document.removeEventListener('mousemove', (e) => this.onMouseMove);
-    this.setState({isTopDragging: false});
-  }
+    this.setState({ isTopDragging: false });
+  };
 
   onMouseMove = e => {
     if (!this.state.isTopDragging) return;
     const newTopTextLeft = this.state.topTextLeft + e.movementX;
     const newTopTextTop = this.state.topTextTop + e.movementY;
-    this.setState({topTextLeft: newTopTextLeft, topTextTop: newTopTextTop});
-  }
+    this.setState({ topTextLeft: newTopTextLeft, topTextTop: newTopTextTop });
+  };
+
+  onMouseDownBottom = e => {
+    this.setState({ isBottomDragging: true });
+    if(this.state.bottomListener == true){
+      document.addEventListener("mousemove", e => this.onMouseMoveBottom(e), this.setState({bottomListener: false}));
+    }
+  };
+
+  onMouseUpBottom = e => {
+    console.count();
+    document.removeEventListener("mousemove", e => this.onMouseMoveBottom(e));
+    this.setState({ isBottomDragging: false });
+  };
+
+  onMouseMoveBottom = e => {
+    if (!this.state.isBottomDragging) return;
+    const newBottomTextLeft = this.state.bottomTextLeft + e.movementX;
+    const newBottomTextBottom = this.state.bottomTextBottom - e.movementY;
+    this.setState({ bottomTextLeft: newBottomTextLeft, bottomTextBottom: newBottomTextBottom });
+  };
 
   render() {
     const { currentImg, topText, bottomText, fontSize } = this.props;
 
     return (
       <div className="preview-container">
-        <span className="top-text" style={{left: this.state.topTextLeft, top: this.state.topTextTop, fontSize}} onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp}>{topText}</span>
+        <span
+          className="top-text"
+          style={{
+            left: this.state.topTextLeft,
+            top: this.state.topTextTop,
+            fontSize
+          }}
+          onMouseDown={this.onMouseDown}
+          onMouseUp={this.onMouseUp}
+        >
+          {topText}
+        </span>
         <img className="preview-img" src={currentImg} />
-        <span className="bottom-text" style={{ fontSize }}>
+        <span className="bottom-text" style={{ left: this.state.bottomTextLeft, bottom: this.state.bottomTextBottom, fontSize }} onMouseDown={this.onMouseDownBottom} onMouseUp={this.onMouseUpBottom}>
           {bottomText}
         </span>
       </div>
