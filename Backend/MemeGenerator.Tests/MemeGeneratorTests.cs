120bda4dd2aa2c83948f243e5eaf187911bc55fc
Michael Manville <monstro33@yahoo.com>
2019-03-26 10:57:15 -0400

    Backend Setup

---
+++ Backend/MemeGenerator.Tests/MemeGeneratorTests.cs
@@ -0,0 +1,40 @@
+using MemeGenerator.Controllers;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace MemeGenerator.Tests
+{
+    public class MemeGeneratorTests
+    {
+        [Fact]
+        public void Get_Returns_A_List_Of_Memes()
+        {
+            var underTest = new MemeController();
+
+            var result = underTest.Get();
+
+            Assert.Equal(4, result.Value.Count());
+        }
+
+        [Fact]
+        public void Post_Creates_New_Meme()
+        {
+            var underTest = new MemeController();
+
+            var result = underTest.Post("Hello World");
+
+            Assert.True(result.Value);
+        }
+
+        [Fact(Skip = "Post should increase number of memes")]
+        public void Post_Should_Increase_Memes_Count()
+        {
+            var underTest = new MemeController();
+
+            var result = underTest.Get();
+
+            Assert.Equal(4, result.Value.Count());
+        }
+    }
+}
---
+++ Backend/MemeGenerator/Controllers/MemeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemeGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemeController : ControllerBase
+    {
+        private static List<string> memes = new List<string>()
+        {
+            "/Images/Batman.jpg",
+            "/Images/Spiderman.jpg",
+            "/Images/Ancient-Aliens.jpg",
+            "/Images/Bad-Luck-Brian.jpg"
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return memes;
+        }
+
+        [HttpPost]
+        public ActionResult<bool> Post([FromBody] string memes)
+        {
+            return true;
+        }
+    }
+}
