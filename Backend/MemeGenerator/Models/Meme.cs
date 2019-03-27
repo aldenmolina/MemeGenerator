1e83dd67cedc28fa046eda48df995f4927c6cbb2
Joe Scheiman <scheiman.joe@gmail.com>
2019-03-27 10:13:10 -0400

    Modified Model

--- Backend/MemeGenerator/Models/Meme.cs
+++ Backend/MemeGenerator/Models/Meme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace MemeGenerator.Models
 {
     public class Meme
     {
+        [Key]
+        public int MemeId { get; set; }
+        public string imagePath { get; set; }
+        public string topText { get; set; }
+        public string bottomText { get; set; }
+        public int topTextX{ get; set; }
+        public int topTextY { get; set; }
+        public int bottomTextX { get; set; }
+        public int bottomTextY { get; set; }
+        public int fontSize { get; set; }
+
     }
 }
