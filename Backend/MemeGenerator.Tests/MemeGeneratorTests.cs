e9493ff22e2838e09ac9988983b65db98d7ee153
Joe Scheiman <scheiman.joe@gmail.com>
2019-03-28 12:20:07 -0400

    Added 2nd Test

--- Backend/MemeGenerator.Tests/MemeGeneratorTests.cs
+++ Backend/MemeGenerator.Tests/MemeGeneratorTests.cs
@@ -1,8 +1,10 @@
 using MemeGenerator.Controllers;
 using MemeGenerator.Models;
 using MemeGenerator.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -19,14 +21,21 @@ namespace MemeGenerator.Tests
             underTest = new MemeController(testRepo);
         }
 
-        [Fact] 
-        public void Get_Returns_A_List_Of_Memes()
+        [Fact]
+        public void Can_Create_a_Meme()
         {
-            //var result = underTest.Get();
-            var newMeme = new Meme();
-            var result = underTest.Post(newMeme);
+            var pass = true;
 
-            Assert.Equal(2, result.Value.Count());
+            var newMeme = new Meme();   //gets added to testRepo
+            var wrongMeme = new Meme(); //doesn't get added to testRepo
+
+            //This is based off NSubstitute website, No Assert Needed! (Received, instead)
+            testRepo.Create(newMeme);
+            if (!pass) //Change var pass to false to see this fail - it works
+                testRepo.Received().Create(wrongMeme);
+
+            if (pass)
+                testRepo.Received().Create(newMeme);
         }
 
         [Fact]
@@ -38,9 +47,25 @@ namespace MemeGenerator.Tests
             Assert.True(result.Value);
         }
 
-        [Fact(Skip = "Post should increase number of memes")]
+
+        [Fact] 
+        public void Get_Returns_A_List_Of_Memes()
+        {
+            var expectedModel = new List<Meme>();
+            testRepo.GetAll().Returns(expectedModel);
+            var newMeme = new Meme();
+            var actualModelx = underTest.Post(newMeme);
+            var actualModel = underTest.Get();
+            
+            //Assert.Equal()
+            Assert.IsType<ActionResult<List<Meme>>>(actualModel);
+        }
+
+        
+        [Fact] //(Skip = "Post should increase number of memes")
         public void Post_Should_Increase_Memes_Count()
         {
+            underTest.Post(new Meme());
             var result = underTest.Get();
 
             Assert.Equal(4, result.Value.Count());
